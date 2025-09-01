05c7d11ae5bacc6572613f95e0892272c47ddb9c
Sergio Afonso <saafonsos@gmail.com>
2025-09-01 11:14:51 +0200

    Add Fact tables

---
+++ models/gold/fact_orders.sql
@@ -0,0 +1,20 @@
+{{ config(
+    materialized='table',
+    schema='GOLD'
+) }}
+
+select
+    o.O_ORDERKEY  as ORDERKEY,
+    o.C_CUSTKEY   as CUSTOMER_KEY,
+    o.O_ORDERDATE as ORDER_DATE,
+    sum(l.L_QUANTITY)        as TOTAL_QUANTITY,
+    sum(l.L_EXTENDEDPRICE)   as TOTAL_PRICE,
+    sum(l.L_EXTENDEDPRICE * (1 - l.L_DISCOUNT)) as TOTAL_NET_SALES,
+    sum(l.L_EXTENDEDPRICE * (1 - l.L_DISCOUNT) * (1 + l.L_TAX)) as TOTAL_GROSS_SALES
+from {{ ref('orders') }} o
+join {{ ref('lineitem') }} l
+  on o.O_ORDERKEY = l.L_ORDERKEY
+group by
+    o.O_ORDERKEY,
+    o.C_CUSTKEY,
+    o.O_ORDERDATE
